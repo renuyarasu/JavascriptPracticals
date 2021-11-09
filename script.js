bee29c198540c62022c70d0f7fe5dc91209ac02f
Renuka Prasad <renu.yarasu@gmail.com>
2021-11-09 19:04:28 +0530

    Objects

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-var imageArray = ["images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg"];
-var i = 1;
-var ref;
+// Javascript Objects Practical
 
-function imageGallery() {
-    document.images[0].src = imageArray[i];
-    i++;
-    if (i < 4)
-        i = 0;
+let tutorial = {
+    topic: 'JavaScript',
+    trainer: 'VedaGna',
+    duration: 10
 }
 
-function start () {
-    ref = setInterval('imageGallery()', 1000);
-}
-function stop() {
-    clearInterval(ref);
-}
+console.log(`Topic: ${tutorial.topic}`);
+console.log(`Topic: ${tutorial.trainer}`);
+console.log(`Topic: ${tutorial.duration}`);
