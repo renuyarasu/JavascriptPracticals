5cc13eb8705b57edaa80741769497dbbbc8bb520
Renuka Prasad <renu.yarasu@gmail.com>
2021-11-10 19:22:12 +0530

    Q/A/Example: Floating point math - 5

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 console.clear();
 // Eagle Eye Problem in JavaScript 
- 
-const length = 4;
-const numbers = [];
 
-for (var i = 0; i < length; i++) {
-    numbers.push(i + 1)
-}
-console.log(numbers);
+const a = 0.1;
+const b = 0.2;
+const c = 0.3;
+console.log(a + b === c);
+
+
+/* The sum of 0.1 and 0.2 numbers is not exactly 0.3, but slightly above 0.3.
+
+Due to how floating point numbers are encoded in binary, operations like addition of floating point numbers are subject to rounding errors.
+
+Simply put, comparing floats directly is not precise.
+
+Thus 0.1 + 0.2 === 0.3 is false.
+
+Check 0.30000000000000004.com for more information. */
