82e12bf453d44ba54f58d40adb3d29b1d2c2bd05
Renuka Prasad <renu.yarasu@gmail.com>
2021-11-10 13:36:05 +0530

    Q/A/Example: Variable Hoisting

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let x = String({})
+// Variable Hoisting
 
-console.log(x); // [object Object]
-console.log(typeof (x)); // number
+var number = 10;
+
+const display = () => {
+    console.log(number);
+    var number = 10;
+}
+display(); // undefined
+
+// Variable hoisting means the JavaScript engine moves the variable declarations to the top of the script
