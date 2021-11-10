45ab496431b11d7a52de1a9525f844972516e6e1
Renuka Prasad <renu.yarasu@gmail.com>
2021-11-10 11:46:19 +0530

    [] + []

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// Javascript Objects Practical
+let x = [] + [];
 
-let tutorial = {
-    topic: 'JavaScript',
-    trainer: 'VedaGna',
-    duration: 10
-}
-
-console.log(`Topic: ${tutorial.topic}`);
-console.log(`Topic: ${tutorial.trainer}`);
-console.log(`Topic: ${tutorial.duration}`);
-
-let text = '';
-let x;
-for (x in tutorial) {
-    text += tutorial[x] + ', '
-}
-document.write(text)
+console.log(typeof (x)); // string
